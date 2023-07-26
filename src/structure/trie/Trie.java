6fc321e12654d8cfd299b39ede01a7018dfe50a4
cuijianlei <thundersmile@126.com>
2023-07-26 17:00:42 +0800

    add trie

---
+++ src/structure/trie/Trie.java
@@ -0,0 +1,38 @@
+package structure.trie;
+
+import java.util.TreeMap;
+
+/**
+ * 字典树
+ */
+public class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie() {
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(String word) {
+
+    }
+}
