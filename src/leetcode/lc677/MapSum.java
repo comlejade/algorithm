60a3f60f6599bbf9fd6e5b4630b65ad792c271d6
cuijianlei <thundersmile@126.com>
2023-07-27 16:53:04 +0800

    add trie

---
+++ src/leetcode/lc677/MapSum.java
@@ -0,0 +1,43 @@
+package leetcode.lc677;
+
+import leetcode.lc211.WordDictionary;
+
+import java.util.TreeMap;
+
+public class MapSum {
+    private class Node {
+        public int value;
+        public TreeMap<Character, Node> next;
+
+        public Node(int value) {
+            this.value = value;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(0);
+        }
+    }
+
+    private Node root;
+
+    public MapSum() {
+        root = new Node();
+    }
+
+    public void insert(String key, int val) {
+        Node cur = root;
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.value = val;
+    }
+
+    public int sum(String prefix) {
+
+    }
+}
--- src/structure/trie/Trie.java
+++ src/structure/trie/Trie.java
@@ -1,5 +1,9 @@
 package structure.trie;
 
+import structure.set.BSTSet;
+import utils.FileOpreation;
+
+import java.util.ArrayList;
 import java.util.TreeMap;
 
 /**
@@ -32,7 +36,93 @@ public class Trie {
         return size;
     }
 
+    // 向Trie中添加一个新的单词word
     public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size++;
+        }
+    }
+
+    // 查询单词word是否在Trie中
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    // 查询是否在Trie中有单词以prefix为前缀
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return false;
+            }
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("双城记");
+
+        ArrayList<String> words = new ArrayList<>();
+
+        if (FileOpreation.readFile("src/utils/shuang.txt", words)) {
+            long startTime = System.nanoTime();
+
+            BSTSet<String> set = new BSTSet<>();
+            for (String word: words) {
+                set.add(word);
+            }
+
+            for (String word : words) {
+                set.contains(word);
+            }
+
+            long endTime = System.nanoTime();
+
+            double time = (endTime - startTime) / 1000000000.0;
+
+            System.out.println("Total different words: " + set.getSize());
+            System.out.println("BSTSet: " + time + " s");
+
+
+            long startTime1 = System.nanoTime();
+
+            Trie trie = new Trie();
+            for (String word: words) {
+                trie.add(word);
+            }
+
+            for (String word : words) {
+                trie.contains(word);
+            }
+
+            long endTime1 = System.nanoTime();
+
+            double time1 = (endTime1 - startTime1) / 1000000000.0;
+
+            System.out.println("Total different words: " + set.getSize());
+            System.out.println("Trie: " + time1 + " s");
+        }
+
+
 
     }
 }
